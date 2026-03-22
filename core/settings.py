14ff86ab4789bdcedc958359982de3ca94403d51
Roger Juarez\roger <roger.juarez.rdjjm@gmail.com>
2026-03-22 01:08:03 -0600

    Corregido STATIC_URL con barra inicial

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a#c1je#s(zhy95aeuwr5t(bevr9ac(-gz9_55=jw7cja_n1gns'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
